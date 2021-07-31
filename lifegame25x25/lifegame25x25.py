7d46b0d294d641223fe7af8312fe2f41fb2250a9
itokonpsp <itokonpsp@gmail.com>
2021-08-01 01:28:02 +0900

    描画と初期フィールドの生成を追加

--- lifegame25x25/lifegame25x25.py
+++ lifegame25x25/lifegame25x25.py
@@ -1,1 +1,22 @@
+import os
+import random
 
+def generate_field():
+    field = list()
+    for _ in range(25):
+        field.append( [random.choice([True, False]) for _ in range(25)] )
+    return field
+
+
+def draw(field):
+    os.system('cls')
+    for row in field:
+        for cell in row:
+            if cell:
+                print('■', end='')
+            else:
+                print('□', end='')
+        print()
+
+if __name__ == '__main__':
+    draw(generate_field())
